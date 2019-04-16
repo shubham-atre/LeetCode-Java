25ee5b883e63682aafaae3be8dc054de77bf5c24
shubham-atre <satre@cs.stonybrook.edu>
2019-04-16 19:21:21 -0400

    Create WordLadder.java

---
+++ src/WordLadder.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordDic) {
+        HashSet<String> wordDict = new HashSet<>(wordDic);
+        Set<String> reached = new HashSet<String>();
+        reached.add(beginWord);
+        if(!wordDict.contains(endWord)) return 0;
+        int distance = 1;
+        while (!reached.contains(endWord)) {
+            Set<String> toAdd = new HashSet<String>();
+            for (String each : reached) {
+                for (int i = 0; i < each.length(); i++) {
+                    char[] chars = each.toCharArray();
+                    for (char ch = 'a'; ch <= 'z'; ch++) {
+                        chars[i] = ch;
+                        String word = new String(chars);
+                        if (wordDict.contains(word)) {
+                            toAdd.add(word);
+                            wordDict.remove(word);
+                        }
+                    }
+                }
+            }
+            distance++;
+            if (toAdd.size() == 0) return 0;
+            reached = toAdd;
+        }
+        return distance;
+    }
+}
